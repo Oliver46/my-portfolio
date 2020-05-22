b2fa5a8ddfe35cd454f499e410f49b6c7e902b09
Oliver <oliver46leon@hotmail.com>
2020-05-22 14:51:31 -0400

    added modal

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,8 +1,48 @@
 import '../style/style.scss';
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
+//import modal from './script/modal.js';
+
+
+//get modal element
+let modal = document.getElementById('simpleModal');
+//get open modal button
+let modalBtn = document.getElementById('modalBtn');
+//get close button 
+let closeBtn = document.getElementById('closeBtn');
+
+//listen for open click
+modalBtn.addEventListener('click', openModal);
+
+//listen for close click
+closeBtn.addEventListener('click', closeModal);
+
+//Listen for ouside click
+window.addEventListener('click', clickOutside);
+
+//function to open modal
+function openModal(){
+   modal.style.display = 'block';
+}
+
+//function to close modal
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+//function to close modal if outside click
+function clickOutside(e){
+    console.log(e.target);
+    //modal = dark area around the white box
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+   
+}
+
 
 if(module.hot){
     module.hot.accept()
---
+++ app/src/js/scripts/modal.js
@@ -0,0 +1,1 @@
+alert("Hello Oliver Leon");
